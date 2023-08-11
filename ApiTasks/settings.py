d0fb3b90997a3c2a78eddadfe75b75d13fe6a3a1
Frank Grijalba <77861241+Frank-Grijalba@users.noreply.github.com>
2023-08-10 22:51:03 -0500

    Update model

--- ApiTasks/settings.py
+++ ApiTasks/settings.py
@@ -132,14 +132,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-co'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Colombia/Bogota'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
+DATE_INPUT_FORMATS = ['%y-%m-%d %h:%m']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
