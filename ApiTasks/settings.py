4ff493d0c6737baa66cd40f4fa9f67331ec4f641
Frank Grijalba <77861241+Frank-Grijalba@users.noreply.github.com>
2023-08-10 19:28:00 -0500

    Fix error

--- ApiTasks/settings.py
+++ ApiTasks/settings.py
@@ -27,11 +27,11 @@ SECRET_KEY = environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = 'RENDER' not in os.environ
+DEBUG = 'RENDER' not in environ
 
 ALLOWED_HOSTS = []
 
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+RENDER_EXTERNAL_HOSTNAME = environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
